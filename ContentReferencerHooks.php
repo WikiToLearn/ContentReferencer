f93e5c44f764eb3a497da04153764e2f870f4768
Russell Greene <russellgreene8@gmail.com>
2016-01-17 17:16:17 -0700

    Make it more generic: all it takes now is just a id and content--styling will come in templates

--- ContentReferencerHooks.php
+++ ContentReferencerHooks.php
@@ -7,39 +7,21 @@ if ( !defined( 'MEDIAWIKI' ) ){
 class ContentReferencer {
     
     function onParserFirstCallSetup( Parser $parser ) {
-    
+        
         $parser->setHook( 'content', 'ContentReferencer::parse_content' );
         $parser->setHook( 'content-ref', 'ContentReferencer::parse_content_ref' );
         
     }
     
     function parse_content( $input, $args, Parser $parser, PPFrame $frame ) {
-        
-        $id = $args['id'];
-        $name = $args['name'];
-        
-        // wrap in div
-        $text = Html::rawElement( 'div', '' , $parser->recursiveTagParse( $input, $frame ) );
-        
-        $header = Html::rawElement( 'strong', '', "Figure $id: $name" . "<hr />" );
-        $header = Html::rawElement( 'div', '', "$header" );
-        
-        $text = Html::rawElement( 'blockquote', array( 'id' => $id), $header . $text );
-        
-        return $text;
+                
+        return Html::rawElement( 'div', array( 'id' => $args['id'] ) , $parser->recursiveTagParse( $input, $frame ) );
         
     }
     
     function parse_content_ref( $input, $args, Parser $parser, PPFrame $frame ) {
-        
-        $id = $args['id'];
-        
-        
-        return $parser->recursiveTagParse('[[' . $args['page'] . "#$id" . '|' . $input . ']]');
-        
-        
-        
-        
+                
+        return $parser->recursiveTagParse('[[' . $args['page'] . "#$args['id']" . '|' . $input . ']]');
         
     }
 }
