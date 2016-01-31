d90b0024f30b69c085ce8db3a57dcdfe6b8a7527
Russell Greene <russellgreene8@gmail.com>
2016-01-30 20:59:08 -0700

    Use page save to write to the database. There is still an issue: you need to save it twice, but this is only with links on the same page being defined for the first time.

--- ContentReferencerHooks.php
+++ ContentReferencerHooks.php
@@ -9,9 +9,12 @@ require_once ('ContentReferencerTableName.php');
 
 class ContentReferencer {
     
+    
+    const GetLabelsRegex = "/<\\s*nowiki\\s*>.*<\\s*\\/\\s*nowiki\\s*>|<\\s*label\\s[^>]*label\\s*=\\s*([\"'](.*)[\"']|([\\w-]+))/";
+    
     static function onParserFirstCallSetup( Parser &$parser ) {
-        $parser->setHook( 'content', 'ContentReferencer::parse_content' );
-        $parser->setHook( 'content-ref', 'ContentReferencer::parse_content_ref' );
+        $parser->setHook( 'label', 'ContentReferencer::parse_label' );
+        $parser->setHook( 'ref', 'ContentReferencer::parse_ref' );
     }
     
     // deletes all the references that it uses
@@ -33,50 +36,82 @@ class ContentReferencer {
         
     }
     
-    static function parse_content( $input, array $args, Parser $parser, PPFrame $frame ) {
-        // TODO: is there a better place to put the database insertion code? Maybe on page save is more approiate...
-        // get the database
-        $DB = wfGetDB(DB_MASTER);
-        $DB->begin();
+    public static function onPageContentSave(
+        WikiPage &$wikiPage, User &$user, Content &$content, &$summary, $isMinor, $isWatch, $section, &$flags, Status &$status ) {
         
-        // make sure that the table is defined--or else the user needs to run maintenance/SetupDatabase.php
-        if(!$DB->tableExists(ContentReferencerTableName)) {
-            die("Please run maintenance/SetupDatabase.php to setup the tables");
+        // get the current tags from the page
+        /////////////////////////////////////
+        $CurrentReferences;
+        {
+            preg_match_all(self::GetLabelsRegex, $wikiPage->getText(), $matches);
+            // build it as a "set"
+            foreach(array_merge( $matches[2], $matches[3] ) as $toAdd) {
+                if($toAdd != '')
+                    $CurrentReferences[$toAdd] = 1;
+            }
         }
         
-        $ThisPageName = (string) $parser->getTitle();
-        $ID = $args['id'];
         
-        // see if the reference already exists and warn if it does
-        try {
-            $ExistingReference = $DB->select(ContentReferencerTableName, "reference_page_name", "reference_name='$ID'");
-        } catch(Exception $e) {
-            die($e->getMessage());
+        // get the old tags from the database
+        /////////////////////////////////////
+        $OldReferences;
+        {
+            if (($rev = $wikiPage->getRevision()) != null && ($prevRev = $rev->getPrevious()) != null ) {
+                preg_match_all(self::GetLabelsRegex, $prevRev->getText(), $matches);// build it as a "set"
+                foreach(array_merge( $matches[2], $matches[3] ) as $toAdd) {
+                    if($toAdd != '')
+                        $OldReferences[$toAdd] = 1;
+                }
+            }
         }
         
-        $shouldwrite = true;
-        foreach($ExistingReference as $RefPageName) {
-            $shouldwrite = false;
+        // Resolve differences
+        //////////////////////
+        $DB = wfGetDB(DB_MASTER);        
+        $DB->begin(); // start a transaction
+        // start with the newOnes and make sure that the database is compatable, and remove them 
+        foreach(array_keys($CurrentReferences) as $RefToMakeSureExists) {
+            if(isset($OldReferences[$RefToMakeSureExists])) {
+                // if it is already in the array, then we don't have to worry about it; delete it
+                unset($OldReferences[$RefToMakeSureExists]);
+            } else {
+                // if it doesn't exist, we need to add it
+                try {
+                    $DB->insert(ContentReferencerTableName, 
+                        array('reference_name' => $RefToMakeSureExists, 'reference_page_name' => (string)$wikiPage->getTitle()));
+                } catch (Exception $e) {
+                    die($e->getMessage());
+                }
+            }
         }
-        $DB->freeResult($ExistingReference);
         
-        if($shouldwrite) {
-            // write to the database
-            try{
-                $DB->insert(ContentReferencerTableName, array('reference_name' => $ID, 'reference_page_name' => $ThisPageName));
+        
+        
+        // now, anything left in $OldReferences has been deleted. Let's remove it from the database
+        foreach(array_keys($OldReferences) as $RefToDelete) {
+            try {
+                $DB->delete(ContentReferencerTableName, "reference_name='$RefToDelete'");
             } catch(Exception $e) {
                 die($e->getMessage());
             }
         }
         
-        $DB->commit();
+        $DB->commit(); // end the transaction
+        
+    }
+
+    
+    static function parse_label( $input, array $args, Parser $parser, PPFrame $frame) {        
+        
+        $ThisPageName = (string) $parser->getTitle();
+        $ID = $args['label'];
         
         // return a id-ed span of the parsed input
         return Html::rawElement('span', array('id' => $ID), $parser->recursiveTagParse($input, $frame));
         
     }
     
-    static function parse_content_ref( $input, array $args, Parser $parser, PPFrame $frame ) {
+    static function parse_ref( $input, array $args, Parser $parser, PPFrame $frame ) {
         
         // get the database
         $DB = wfGetDB(DB_SLAVE);
@@ -86,7 +121,7 @@ class ContentReferencer {
             die("Please run maintenance/SetupDatabase.php to setup the tables");
         }
         
-        $ID = $args['id'];
+        $ID = $args['label'];
         
         try {
             // select it from the database
